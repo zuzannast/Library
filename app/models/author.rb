05913b441a00f4ad917fdd85942fbf6f3aa395f4
zustmust <zuzanna.st@gmail.com>
2014-01-25 16:39:54 +0100

    Funkcjonalnosci na 3 gotowe

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,12 @@
 class Author < ActiveRecord::Base
-	has_many :books
-	def full_name
-		"#{self.first_name} #{self.last_name}"
-	end
+  has_many :books 
+  def full_name
+     "#{self.first_name} #{self.last_name}"
+  end
+    
+ransacker :full_name do |parent|
+  Arel::Nodes::InfixOperation.new('||',
+  Arel::Nodes::InfixOperation.new('||', parent.table[:first_name], ' '),
+  parent.table[:last_name])
+end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
   belongs_to :author
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories, join_table: :books_categories
+
+  validates :title, :presence => true
 end
